e58493a443b2b748cba9096e6cae00e1714c52ab
Abhishek Tripathi <Abhishek-72>
2024-12-05 12:24:44 +0530

    userauthentication done

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,14 @@ const express = require("express");
 const userRoute = require("./routes/userRoute.js");
 const cors = require("cors");
 const connectToDb = require("./Db/dB.js");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 connectToDb();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/userController.js");
+const authMiddleware = require("../middlewares/authMiddleware.js");
 
 router.post(
   "/register",
@@ -19,5 +20,19 @@ router.post(
   ],
   userController.resgisterUser
 );
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 charcters long"),
+  ],
+  userController.loginUser
+);
+
+router.get("/profile", authMiddleware.authUser, userController.getUserProfile);
+router.get("/logout", authMiddleware.authUser, userController.logoutUser);
 
 module.exports = router;
